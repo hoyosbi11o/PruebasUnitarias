d15f1ef1616693f76ed0f63e83d4a9e9297a775b
angel <hoyosbolivara11@gmail.com>
2023-11-13 20:30:36 -0500

    third test commit

--- __test__/ValorAbsoluto.test.js
+++
@@ -1,7 +0,0 @@
-const ValorAbsoluto=require('../src/ValorAbsoluto')
-
-describe("Prueba del Valor absoluto",()=>{
-    test('5 is 5',()=>{
-        expect(ValorAbsoluto(5)).toBe(5)
-    })
-})
--- src/ValorAbsoluto.js
+++
@@ -1,4 +0,0 @@
-function suma(a) {
-    return a
-}
-module.exports=suma
